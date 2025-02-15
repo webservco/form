3f20a78c862d9a610d9de12b4c4ebe9f0a35a484
Radu Maier <ariademur@webserv.co>
2025-02-15 11:44:41 +0100

    handleFormField

--- src/WebServCo/Form/Service/HtmlPostForm.php
+++ src/WebServCo/Form/Service/HtmlPostForm.php
@@ -8,10 +8,14 @@ use Error;
 use Fig\Http\Message\RequestMethodInterface;
 use Fig\Http\Message\StatusCodeInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use WebServCo\Form\Contract\FormFieldInterface;
 use WebServCo\Form\Contract\FormInterface;
 
 use function array_key_exists;
 use function is_array;
+use function is_scalar;
+use function sprintf;
+use function strval;
 
 final class HtmlPostForm extends AbstractForm implements FormInterface
 {
@@ -50,15 +54,34 @@ final class HtmlPostForm extends AbstractForm implements FormInterface
          * it simply only process stuff that we need.
          */
         foreach ($this->getFields() as $formField) {
-            $id = $formField->getId();
-            if (!array_key_exists($id, $parsedBody)) {
-                continue;
-            }
-
-            $formField->setValue((string) $parsedBody[$id]);
+            $this->handleFormField($formField, $parsedBody);
         }
 
         // Filter and validate each field.
         return $this->processForm();
+    }
+
+    /**
+     * @phpcs:ignore SlevomatCodingStandard.TypeHints.DisallowMixedTypeHint.DisallowedMixedTypeHint
+     * @param array<mixed> $parsedBody
+     */
+    private function handleFormField(FormFieldInterface $formField, array $parsedBody): bool
+    {
+        $id = $formField->getId();
+        if (!array_key_exists($id, $parsedBody)) {
+            return false;
+        }
+
+        $value = $parsedBody[$id];
+        if (!is_scalar($value)) {
+            $this->addError(new Error(
+                sprintf('Data is not a scalar for field "%s".', $id),
+                StatusCodeInterface::STATUS_BAD_REQUEST,
+            ),);
+
+            return false;
+        }
+
+        return $formField->setValue(strval($value));
     }
 }
